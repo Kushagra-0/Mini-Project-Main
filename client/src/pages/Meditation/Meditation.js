91b4cc8d215cce42b8ca1ce737a7e63e5598d10f
baba-shashank <shashanknayak0702@gmail.com>
2023-12-03 18:50:33 +0530

    Improved Frontend

--- client/src/pages/Meditation/Meditation.js
+++ client/src/pages/Meditation/Meditation.js
@@ -1,10 +1,76 @@
-import React from 'react'
-import NavBar from '../../components/Navbar/Navbar'
+import React, { useState, useEffect } from 'react'
+import './Meditation.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Timer from '../../components/Timer/Timer'
+import Meditation from '../../components/Meditation/Meditation'
+import SettingsMenu from '../../components/SettingsMenu/SettingsMenu'
+import Footer from '../../components/Footer/Footer'
+import Nav from '../../components/Nav/Nav'
 
-export default function Meditation() {
+function App() {
+  const [showSettings, setShowSettings] = useState(false)
+  const [showMeditation, setShowMeditation] = useState(false)
+  const [reset, setReset] = useState(false)
+  const [skip, setSkip] = useState()
+  const [intervalCount, setIntervalCount] = useState(0)
+  const [sessionCount, setSessionCount] = useState(1)
+
+  const triggerReset = () => setReset(reset => !reset)
+
+  const triggerSkip = () => {
+    setSkip(true)
+  }
+
+  const openMeditation = () => {
+    let timer = document.querySelector('#timer')
+
+    timer.style.position = timer.style.position === 'absolute'
+      ? 'relative'
+      : 'absolute'
+
+    setShowMeditation(!showMeditation)
+  }
+
+  useEffect(() => {
+    if (intervalCount >= 5) {
+      setIntervalCount(1)
+      setSessionCount(sessionCount + 1)
+    }
+  }, [intervalCount])
+
   return (
-    <div>
-      <NavBar />
+    <div className='medi'>
+    <div id="app">
+      <Navbar
+        openSettings={() => setShowSettings(!showSettings)}
+        openMeditation={openMeditation}
+        hideIcons={showSettings}
+        isMeditation={showMeditation}
+        triggerReset={triggerReset}
+        intervalCount={intervalCount}
+        sessionCount={sessionCount}
+      />
+      {showSettings
+        ? <SettingsMenu />
+        : null
+      }
+      {showMeditation
+        ? <Meditation />
+        : null
+      }
+      <Timer
+        hidden={showSettings || showMeditation}
+        reset={reset}
+        setIntervalCount={setIntervalCount}
+        intervalCount={intervalCount}
+        skip={skip}
+        setSkip={setSkip}
+        openMeditation={openMeditation}
+      />
+      <Footer hidden={showSettings || showMeditation} triggerSkip={triggerSkip} />
+    </div>
     </div>
   )
 }
+
+export default App
